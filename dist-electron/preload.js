d5b65e2ec08963eb47f2fe1c9f863c5ff4ea912c
yobwweh <yobwweh@senikoservices.com>
2026-02-27 12:57:13 +0000

    chore: update .gitignore and remove tracked binaries/env files

--- dist-electron/preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("api", {
-  checkUpdates: () => ipcRenderer.invoke("check-updates"),
-  getOpenShift: (userId) => ipcRenderer.invoke("get-open-shift", userId),
-  openShift: (userId, openingBalance) => ipcRenderer.invoke("open-shift", { userId, openingBalance }),
-  closeShift: (shiftId, actualCashCount) => ipcRenderer.invoke("close-shift", { shiftId, actualCashCount }),
-  processSale: (payload) => ipcRenderer.invoke("process-sale", payload)
-});
